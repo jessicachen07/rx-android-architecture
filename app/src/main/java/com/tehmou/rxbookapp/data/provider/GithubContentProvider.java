50cb3bcea9bf694dfb2efe51bbb7799bcb946ba7
Timo Tuominen <timo.tuominen@gmail.com>
2015-06-03 19:36:27 +0100

    Add builder for DatabaseRouteBase

--- app/src/main/java/com/tehmou/rxbookapp/data/provider/GithubContentProvider.java
+++ app/src/main/java/com/tehmou/rxbookapp/data/provider/GithubContentProvider.java
@@ -1,7 +1,13 @@
 package com.tehmou.rxbookapp.data.provider;
 
+import android.net.Uri;
+
 import com.tehmou.rxandroidarchitecture.contract.DatabaseContract;
+import com.tehmou.rxandroidarchitecture.contract.SerializedJsonContract;
 import com.tehmou.rxandroidarchitecture.provider.ContractContentProviderBase;
+import com.tehmou.rxandroidarchitecture.route.DatabaseRouteBase;
+
+import rx.functions.Func1;
 
 /**
  * Created by ttuo on 22/03/15.
@@ -12,25 +18,51 @@ public class GithubContentProvider extends ContractContentProviderBase {
     private static final int DATABASE_VERSION = 12;
 
     public GithubContentProvider() {
+        final Func1<Uri, String> getWhereFuncInteger =
+                uri -> SerializedJsonContract.ID + " = " + uri.getLastPathSegment();
+        final Func1<Uri, String> getWhereFuncString =
+                uri -> SerializedJsonContract.ID + " = '" + uri.getLastPathSegment() + "'";
+        final String defaultSortOrder = SerializedJsonContract.ID + " ASC";
+
         DatabaseContract gitHubRepositoryContract = new GitHubRepositoryContract();
         addDatabaseContract(gitHubRepositoryContract);
         addDatabaseRoute(
-                new GitHubRepositorySingleRoute(gitHubRepositoryContract.getTableName()));
+                new DatabaseRouteBase.Builder(gitHubRepositoryContract.getTableName())
+                        .setMimeType("vnd.android.cursor.item/vnd.tehmou.android.rxbookapp.repository")
+                        .setDefaultSortOrder(defaultSortOrder)
+                        .setGetWhereFunc(getWhereFuncInteger)
+                        .setPath(gitHubRepositoryContract.getTableName() + "/*")
+                        .build());
 
         DatabaseContract gitHubRepositorySearchContract = new GitHubRepositorySearchContract();
         addDatabaseContract(gitHubRepositorySearchContract);
         addDatabaseRoute(
-                new GitHubRepositorySearchSingleRoute(gitHubRepositorySearchContract.getTableName()));
+                new DatabaseRouteBase.Builder(gitHubRepositorySearchContract.getTableName())
+                        .setMimeType("vnd.android.cursor.item/vnd.tehmou.android.rxbookapp.repositorysearch")
+                        .setDefaultSortOrder(defaultSortOrder)
+                        .setGetWhereFunc(getWhereFuncString)
+                        .setPath(gitHubRepositorySearchContract.getTableName() + "/*")
+                        .build());
 
         DatabaseContract userSettingsContract = new UserSettingsContract();
         addDatabaseContract(userSettingsContract);
         addDatabaseRoute(
-                new UserSettingsSingleRoute(userSettingsContract.getTableName()));
+                new DatabaseRouteBase.Builder(userSettingsContract.getTableName())
+                        .setMimeType("vnd.android.cursor.item/vnd.tehmou.android.rxbookapp.usersettings")
+                        .setDefaultSortOrder(defaultSortOrder)
+                        .setGetWhereFunc(getWhereFuncInteger)
+                        .setPath(userSettingsContract.getTableName() + "/*")
+                        .build());
 
         DatabaseContract networkRequestStatusContract = new NetworkRequestStatusContract();
         addDatabaseContract(networkRequestStatusContract);
         addDatabaseRoute(
-                new NetworkRequestStatusSingleRoute(networkRequestStatusContract.getTableName()));
+                new DatabaseRouteBase.Builder(networkRequestStatusContract.getTableName())
+                        .setMimeType("vnd.android.cursor.item/vnd.tehmou.android.rxbookapp.networkrequeststatus")
+                        .setDefaultSortOrder(defaultSortOrder)
+                        .setGetWhereFunc(getWhereFuncInteger)
+                        .setPath(networkRequestStatusContract.getTableName() + "/*")
+                        .build());
     }
 
     @Override
